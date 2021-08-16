a83b360cc10af9c5e239746f021b292a1764ebf5
Himasha Weerasinghe <himashatw@gmail.com>
2021-08-17 00:32:31 +0530

    server defined

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8080;
+const mongoose = require("mongoose");
+const { MONGO_URL } = require("./keys");
 
-
-
-
-
+app.use(cors());
 app.use(express.json());
 
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
-const PORT = 8080;
+mongoose.connection.on("connected", () =>
+  console.log("Connected to MongoCluster")
+);
+mongoose.connection.on("error", () =>
+  console.log("MongoDB Connection Unsuccessfull")
+);
 
 app.listen(PORT, () => {
   console.log("Server is running on port : " + PORT);
