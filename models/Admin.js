7a2a9a1f68beccbd140f9fbd9c43ee18e32f09f0
Himasha Weerasinghe <himashatw@gmail.com>
2021-08-17 21:09:54 +0530

    Admin Schema added

---
+++ models/Admin.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const AdminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const Admin = mongoose.model("Admin", AdminSchema);
+module.exports = Admin;
