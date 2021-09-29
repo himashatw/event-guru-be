610243d9240731e5c35f11168352a4e2f98d5316
Himasha Weerasinghe <himashatw@gmail.com>
2021-09-29 20:30:02 +0530

    packages model updated

--- models/Package.js
+++ models/Package.js
@@ -1,44 +1,49 @@
 const mongoose = require("mongoose");
 
-const PackageSchema = new mongoose.Schema({
-  packageName: {
-    type: String,
-    required: true,
-    trim: true,
+const PackageSchema = new mongoose.Schema(
+  {
+    packageName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    packageDetails: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    packageImageUrl: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    packageOffer: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    packageVenue: {
+      type: String,
+      required: false,
+    },
+    participants: {
+      type: Number,
+      required: true,
+    },
+    packageType: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    packagePrice: {
+      type: Number,
+      required: true,
+    },
   },
-  packageDetails: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  packageImageUrl: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  packageOffer: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  packageVenue: {
-    type: String,
-    required: false,
-  },
-  participants: {
-    type: Number,
-    required: true,
-  },
-  packageType: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  packagePrice: {
-    type: Number,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Package = mongoose.model("Package", PackageSchema);
 module.exports = Package;
